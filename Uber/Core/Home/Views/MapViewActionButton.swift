91d28b75e45b693bdfb98ef10a4b20e337429f56
babayevkamran23 <kamran.babayev.23@mail.ru>
2024-03-16 11:08:12 +0400

    Added some views to HomeView

---
+++ Uber/Core/Home/Views/MapViewActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewActionButton.swift
+//  Uber
+//
+//  Created by Babayev Kamran on 16.03.24.
+//
+
+import SwiftUI
+
+struct MapViewActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapViewActionButton()
+}
---
+++ Uber/Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchActivationView.swift
+//  Uber
+//
+//  Created by Babayev Kamran on 16.03.24.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchActivationView()
+}
