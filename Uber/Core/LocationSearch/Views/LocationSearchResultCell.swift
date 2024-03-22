800da33aadaa4891529f04b3901821c295c8dfc8
babayevkamran23 <kamran.babayev.23@mail.ru>
2024-03-22 22:19:53 +0400

    Finding coordinates

--- Uber/Core/LocationSearch/Views/LocationSearchResultCell.swift
+++ Uber/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct LocationSearchResultCell: View {
+    
+    let title: String
+    let subtitle: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "mappin.circle.fill")
+                .resizable()
+                .foregroundColor(.blue)
+                .accentColor(.white)
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.body)
+                
+                Text(subtitle)
+                    .font(.system(size: 15))
+                    .foregroundColor(.gray)
+                
+                Divider()
+            }
+            .padding(.leading, 8)
+            .padding(.vertical, 8)
+        }
+        .padding(.leading)
     }
 }
 
 #Preview {
-    LocationSearchResultCell()
+    LocationSearchResultCell(title: "Starbucks", subtitle: "Etibarli MTK")
 }
--- Uber/Core/LocationSearch/Views/LocationSearchView.swift
+++ Uber/Core/LocationSearch/Views/LocationSearchView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct LocationSearchView: View {
+    
+    @State private var startLocationText = ""
+    @Binding var showLocationSearchView: Bool
+    @EnvironmentObject var viewModel: LocationSearchViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            // Header view
+            HStack {
+                
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack {
+                    TextField("Current location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where to?", text: $viewModel.queryFragment)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                }
+                
+            }
+            .padding(.horizontal)
+            .padding(.top, 64)
+            
+            Divider()
+                .padding(.vertical)
+            
+            // List view
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(viewModel.results, id: \.self) { result in
+                    
+                        LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                viewModel.selectLocation(result)
+                                showLocationSearchView.toggle()
+                            }
+                        
+                    }
+                }
+            }
+        }.background(.white)
     }
 }
 
 #Preview {
-    LocationSearchView()
+    LocationSearchView(showLocationSearchView: .constant(true))
 }
